f8a7f2bef9eac3313275fe3d740323cf18962104
natty ayana and sandra castaneda <natty@>
2018-10-18 10:20:14 -0700

    added pics and info for drop ddown of animal wiki

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#turtles2").click(function() {
+    $("#turtles").show();
+  });
+});
+
+$(document).ready(function() {
+  $("#snakes").click(function() {
+    $("#snakes2").show();
+  });
+});
