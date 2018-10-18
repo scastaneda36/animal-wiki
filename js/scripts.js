e8bf08d7ff36b3dfb911bdd66280e43ee212e48f
natty ayana and sandra castaneda <natty+sandra@>
2018-10-18 10:53:34 -0700

    added hide and show function when you click headlines

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
   $("#turtles2").click(function() {
     $("#turtles").show();
+    $("#snakes2").hide();
+    $("#insects2").hide();
   });
 });
 
 $(document).ready(function() {
   $("#snakes").click(function() {
     $("#snakes2").show();
+    $("#turtles").hide();
+    $("#insects2").hide();
+  });
+});
+
+$(document).ready(function() {
+  $("#insects").click(function() {
+    $("#insects2").show();
+    $("#turtles").hide();
+    $("#snakes2").hide();
   });
 });
